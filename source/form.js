87dc946b51e25e1a480ff18f8bf66ab186739dce
rsmith9999 <85262343+rsmith9999@users.noreply.github.com>
2023-03-06 21:31:16 +0000

    Add files via upload

--- source/form.js
+++ source/form.js
@@ -1,20 +1,80 @@
-function submitFormAndRedirect(formId) {
-  const form = document.getElementById(formId);
-  const formAction = form.getAttribute("action");
-  const redirectUrl = form.getAttribute("_next");
-  form.onsubmit = function (event) {
-    // Prevent the default form submission behavior
-    event.preventDefault();
-    // Submit the form data using AJAX
-    const xhr = new XMLHttpRequest();
-    xhr.open(form.method, formAction);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        // Redirect to the specified URL
+// const form = document.getElementById("contactform");
+// const redirectUrl = "https://rsmith9999.github.io/example/thanks.html";
+
+// form.addEventListener("submit", function (event) {
+//   // Prevent the default form submission
+//   event.preventDefault();
+
+//   // Submit the form using AJAX
+//   const formData = new FormData(form);
+//   fetch(form.action, {
+//     method: form.method,
+//     body: formData,
+//   })
+//     .then((response) => {
+//       // Check if the form submission was successful
+//       if (response.ok) {
+//         // Redirect to the specified URL after the form is submitted
+//         window.location.href = redirectUrl;
+//       } else {
+//         console.error("Form submission failed with status " + response.status);
+//         // Handle errors here
+//       }
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//       // Handle errors here
+//     });
+// });
+
+// const form = document.getElementById("contactform");
+// const redirectUrl = "https://rsmith9999.github.io/example/thanks.html";
+
+// form.addEventListener("submit", function (event) {
+//   // Prevent the default form submission
+//   event.preventDefault();
+
+//   // Submit the form using AJAX
+//   const formData = new FormData(form);
+//   fetch(form.action, {
+//     method: form.method,
+//     body: formData,
+//   })
+//     .then((response) => {
+//       // Check if the form submission was successful
+//       if (response.ok) {
+//         // Redirect to the specified URL after the form is submitted
+//         window.location.href = redirectUrl;
+//       } else {
+//         console.error("Form submission failed with status " + response.status);
+//         // Handle errors here
+//       }
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//       // Handle errors here
+//     });
+// });
+
+function submitForm() {
+  const form = document.getElementById("contactform");
+  const redirectUrl = "https://rsmith9999.github.io/example/thanks.html";
+
+  const formData = new FormData(form);
+  fetch(form.action, {
+    method: form.method,
+    body: formData,
+  })
+    .then((response) => {
+      if (response.ok) {
         window.location.href = redirectUrl;
+      } else {
+        console.error("Form submission failed with status " + response.status);
+        // Handle errors here
       }
-    };
-    xhr.send(new FormData(form));
-  };
+    })
+    .catch((error) => {
+      console.error(error);
+      // Handle errors here
+    });
 }
