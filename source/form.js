a51356fb200de11c0ddab9b3aab25893f038742e
rsmith9999 <85262343+rsmith9999@users.noreply.github.com>
2023-03-06 22:08:05 +0000

    Updated test

--- source/form.js
+++ source/form.js
@@ -1,7 +1,6 @@
 function redir() {
-  window.addEventListener("load", () => {
-    setTimeout(() => {
-      window.location.href = "https://rsmith9999.github.io/example2/index.html";
-    }, 5000); // 5000 milliseconds = 5 seconds
-  });
+  setTimeout(() => {
+    window.location.href = "https://rsmith9999.github.io/example2/index.html";
+  }, 5000); // 5000 milliseconds = 5 seconds
 }
+redir();
